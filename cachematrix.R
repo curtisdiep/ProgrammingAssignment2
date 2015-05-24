5ff39b3d405255e087856f77b58b067056d1ad80
Curtis! <curtis.diep1@gmail.com>
2015-05-24 14:57:29 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,34 @@
-##makeCacheMatrix: turns input into square matrix. caches the 
-##inverse then returns the square matrix
-makeCacheMatrix<-function(M) {
-        elements_in_M<-length(M)
-        nrow<-sqrt(elements_in_M)
-        ncol<-nrow
-        matricize<-matrix(M,nrow,ncol)
-        x.inv<-solve(matricize)
-        cache<<-x.inv
-matricize
-                                }
+## Comments on makeCacheMatrix
+## creates a variable, cache, that will be used to 
+## cache matrices. There are 2 functions within 
+## the makeCacheMatrix function. THe first one
+## sets the cache to store the desired matrix.
+## The second retrieves what is in the cache.
 
-##cacheSolve: takes the output of makeCacheMatrix, retrieves 
-##inverse from cache
+makeCacheMatrix<-function(m) {
+  cache<<-NULL
+  set_cache<-function(m) {
+    cache<<-m  
+  }
+  get_cache<-function() cache
+  list(set_cache=set_cache, get_cache=get_cache)
+}
 
-cacheSolve<-function(M){
-        specialM<-makeCacheMatrix(M)
-        specialM.inv<-solve(specialM)
-        if (all.equal(specialM.inv,cache)) 
-{cache}                 
-                        }
+## Comments on cacheSolve
+## searches the cache to see if any matrices
+## can be retrieved. If so, it displays a 
+## message before it returns what is stored
+## in the cache. If the cache is empty,
+## the inverse of the matrix will be solved
+## and stored into the cache.
 
-##Note to editor (I'd appreciate if you could comment on below!)
-
-##Things I didn't understand about this assignment:
-##1)makeCacheMatrix: what is the special matrix?
-##2)cacheSolve: how could the matrix have changed? 
+cacheSolve<-function(m) {
+  x<-makeCacheMatrix()
+  if (!is.null(cache)) {
+    message ("getting cached data")
+    return(cache)
+  }
+    m_inverse<-solve(m)
+    x$set_cache(m_inverse)
+    x$get_cache()
+}
